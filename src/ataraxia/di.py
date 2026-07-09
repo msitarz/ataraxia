e5101ef9ade537e9b64f7138bbc694229f370e6a
Michal Sitarz <28092472+msitarz@users.noreply.github.com>
2026-07-09 10:44:18 -0400

    refactor(di): compute_dependency_tree

--- src/ataraxia/di.py
+++ src/ataraxia/di.py
@@ -14,19 +14,17 @@ def compute_dependency_tree(
     spec: DependencyTreeNode, tree: DependencyTree | None = None
 ) -> DependencyTree:
     """Return dependency tree of `entry_spec`."""
+    deps = () if isinstance(spec, type) else spec.dependencies()
+
     if tree:
         if spec in tree:
             return tree
 
-        if isinstance(spec, type):
-            tree[spec] = ()
-        else:
-            tree[spec] = spec.dependencies()
+        tree[spec] = deps
     else:
-        tree = {spec: ()} if isinstance(spec, type) else {spec: spec.dependencies()}
+        tree = {spec: deps}
 
-    if not isinstance(spec, type):
-        for dep in spec.dependencies():
-            compute_dependency_tree(dep, tree)
+    for dep in deps:
+        compute_dependency_tree(dep, tree)
 
     return tree
