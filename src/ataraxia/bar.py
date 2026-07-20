9b56fcad78c4568a2c2d2e82ed9e988b623a80ef
Michal Sitarz <28092472+msitarz@users.noreply.github.com>
2026-07-20 11:10:38 -0400

    feat(bar): Bar class instantiate from str mapping

--- src/ataraxia/bar.py
+++ src/ataraxia/bar.py
@@ -2,12 +2,33 @@
 # Copyright (C) 2026 by Michal Sitarz
 """Bar dataclass."""
 
+from collections.abc import Mapping
 from dataclasses import dataclass
+import re
+
+DOT_PATTERN = re.compile(r"\.")
 
 
 @dataclass(frozen=True)
 class Bar:
-    """Provides OHLCV bar data."""
+    """OHLCV data container for futures data."""
 
     timestamp: int
+    open: int
+    high: int
+    low: int
     close: int
+    volume: int
+
+    @classmethod
+    def from_map(cls, bar_map: Mapping[str, str]):
+        """Return normalized Bar instantiated from map str values."""
+        return cls(**{k: cls._normalize(v) for k, v in bar_map.items()})
+
+    @classmethod
+    def _normalize(cls, value: str):
+        """Return value as int normalized to ticks."""
+        if DOT_PATTERN.search(value):
+            return int(float(value) * 4)
+        else:
+            return int(value)
---
+++ test/unit/test_bar.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2026 by Michal Sitarz
+import pytest
+
+from ataraxia.bar import Bar
+
+
+@pytest.fixture
+def data():
+    return {
+        "timestamp": "1234",
+        "open": "10.25",
+        "high": "30.50",
+        "low": "10.00",
+        "close": "25.75",
+        "volume": "4321",
+    }
+
+
+def test_bar_normalize():
+    assert Bar._normalize("1234") == 1234
+    assert Bar._normalize("10.25") == int(10.25 * 4)
+
+
+def test_bar_from_map(data):
+    bar = Bar.from_map(data)
+
+    assert bar.timestamp == 1234
+    assert bar.open == int(10.25 * 4)
+    assert bar.high == int(30.50 * 4)
+    assert bar.low == int(10.0 * 4)
+    assert bar.close == int(25.75 * 4)
+    assert bar.volume == 4321
